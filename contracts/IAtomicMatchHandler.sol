9d5fcfeed5b1b367ca257cc98c8c265246c0e19b
defi-guru <86073385+defi-guru@users.noreply.github.com>
2021-06-18 19:39:32 +0500

    init

---
+++ contracts/IAtomicMatchHandler.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.8.0;
+
+interface IAtomicMatchHandler {
+    function onAtomicMatch(address asset, uint256 assetId, uint256 assetAmount, address seller, address buyer, uint256 price) external;
+}
---
+++ contracts/IHasSecondarySaleFees.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.0;
+
+interface IHasSecondarySaleFees {
+    function getFeeRecipients(uint256 id) external view returns (address payable[] memory);
+    function getFeeBps(uint256 id) external view returns (uint[] memory);
+}
